97472cbb1100d68f29ca035a17795a832b6ce9dc
nangs13 <danang130603@gmail.com>
2024-11-05 21:53:56 +0700

    [ADD] Add Inventory controller and it's relative class

---
+++ src/Inventory.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class Inventory {
+    // class Produk nya masih sementara
+    private ArrayList<Produk> produkList;
+    private ArrayList<Kategori> kategoriList;
+
+    public Inventory() {
+        produkList = new ArrayList<>();
+        kategoriList = new ArrayList<>();
+    }
+
+    public boolean cekProduk(String kode) {
+        for (int i = 0; i < produkList.size(); i++) {
+            if (produkList.get(i).getKode().equalsIgnoreCase(kode)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void tampilkanSemuaProduk() {
+        if (produkList.isEmpty()) {
+            System.out.println("Tidak ada produk.");
+        } else {
+            for (Produk produk : produkList) {
+                System.out.println(produk);
+            }
+        }
+    }
+
+    public void tambahProduk(Produk produk) {
+        String kode = produk.getKode();
+        if (!this.cekProduk(kode)) {
+            produkList.add(produk);
+            System.out.println("Produk berhasil ditambahkan.");
+            return;
+        } else {
+            System.out.println("Kode Produk harus unik " + kode);
+            return;
+        }
+    }
+
+    public Produk lihatProduk(String kode) {
+        for (Produk produk : produkList) {
+            if (produk.getKode().equalsIgnoreCase(kode)) {
+                return produk;
+            }
+        }
+        return null;
+    }
+
+    public void ubahProduk(String kode, Produk produkBaru) {
+        String kodeBaru = produkBaru.getKode();
+        for (int i = 0; i < produkList.size(); i++) {
+            if (produkList.get(i).getKode().equalsIgnoreCase(kode)) {
+                if (!this.cekProduk(kodeBaru)) {
+                    produkList.set(i, produkBaru);
+                    System.out.println("Produk berhasil diubah.");
+                    return;
+                } else {
+                    System.out.println("Kode Produk harus unik " + kodeBaru);
+                    return;
+                }
+            }
+        }
+        System.out.println("Produk tidak ditemukan.");
+    }
+
+    public void hapusProduk(String kode) {
+        Produk produk = lihatProduk(kode);
+        if (produk != null) {
+            produkList.remove(produk);
+            System.out.println("Produk berhasil dihapus.");
+        } else {
+            System.out.println("Produk tidak ditemukan.");
+        }
+    }
+}
---
+++ src/InventoryController.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class InventoryController {
+    private Inventory inventory;
+    private Scanner scanner;
+
+    public InventoryController() {
+        inventory = new Inventory();
+        scanner = new Scanner(System.in);
+    }
+
+    public void tambahProduk() {
+        System.out.print("Masukkan kode produk: ");
+        String kode = scanner.nextLine();
+        System.out.print("Masukkan nama produk: ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan kategori produk: ");
+        String kategori = scanner.nextLine();
+        System.out.print("Masukkan stok produk: ");
+        int stok = scanner.nextInt();
+        System.out.print("Masukkan Harga satuan produk: ");
+        double harga = scanner.nextDouble();
+        scanner.nextLine();
+        Produk produk = new Produk(kode, nama, stok, harga, kategori);
+        inventory.tambahProduk(produk);
+    }
+
+    public void tampilkanProduk() {
+        inventory.tampilkanSemuaProduk();
+    }
+
+    public void cariProduk() {
+        System.out.print("Masukkan kode produk yang dicari: ");
+        String kode = scanner.nextLine();
+        Produk produk = inventory.lihatProduk(kode);
+        if (produk != null) {
+            System.out.println("Produk ditemukan: " + produk);
+        } else {
+            System.out.println("Produk tidak ditemukan.");
+        }
+    }
+
+    public void rubahProduk() {
+        System.out.print("Masukkan kode produk yang ingin dirubah: ");
+        String cekKode = scanner.nextLine();
+        Produk oldProduk = inventory.lihatProduk(cekKode);
+
+        if (oldProduk != null) {
+            System.out.println("Produk ditemukan: " + oldProduk);
+        } else {
+            System.out.println("Produk tidak ditemukan.");
+        }
+
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Silahkan masukkan data yang ingin dirubah");
+        System.out.println();
+        System.out.print("Masukkan kode produk: ");
+        String kode = scanner.nextLine();
+        System.out.print("Masukkan nama produk: ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan kategori produk: ");
+        String kategori = scanner.nextLine();
+        System.out.print("Masukkan stok produk: ");
+        int stok = scanner.nextInt();
+        System.out.print("Masukkan Harga satuan produk: ");
+        double harga = scanner.nextDouble();
+        scanner.nextLine();
+        Produk produk = new Produk(kode, nama, stok, harga, kategori);
+
+        inventory.ubahProduk(cekKode, produk);
+    }
+}
